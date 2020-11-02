d9fc9645f097f683ee47e17d94690d9056f2352f
Alexiosdut <Alexiosdut@gmail.com>
2020-11-02 15:46:52 +0200

    CategoryDto: disable case sensitive

--- Dtos/categoryDto.js
+++ Dtos/categoryDto.js
@@ -7,7 +7,7 @@ const CategoryDto = new Schema(
     name: { type: String, required: true, unique: true },
     articles: [{ type: Schema.Types.ObjectId, ref: 'Article' }],
   },
-  { timestamps: true },
+  { timestamps: true, collation: { locale: 'en', strength: 2 } },
 )
 
 module.exports = mongoose.model('Category', CategoryDto)
